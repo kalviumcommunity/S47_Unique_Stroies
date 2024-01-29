0e2e44a122cb88c7299e1c5d595c7bc78493783e
004Anuj <anuj.sahu@kalvium.community>
2024-01-29 18:43:01 +0530

    file added

---
+++ route.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const story = require('./Data.json');
+const app = express();
+const port = 3000;
+
+app.use(express.json()); 
+
+app.get("/", (req, res) => {
+    res.send(story);
+});
+
+app.post('/', (req, res) => {
+    const newStory = {
+        "title": req.body.title,
+        "author": req.body.author,
+        "PlaceOfOrigin": req.body.PlaceOfOrigin
+    };
+    story.push(newStory);
+    res.status(200).send(newStory);
+});
+
+app.put('/:index', (req, res) => {
+    const index = parseInt(req.params.index);
+    const update = req.body;
+    if (index >= 0 && index < story.length) {
+        story[index] = update;
+        res.json(story);
+    } else {
+        res.status(404).json({ error: "Index out of bounds" });
+    }
+});
+
+app.delete('/:index', (req, res) => {
+    const index = parseInt(req.params.index);
+    if (index >= 0 && index < story.length) {
+        story.splice(index, 1);
+        res.json(story);
+    } else {
+        res.status(404).json({ error: "Index out of bounds" });
+    }
+});
+
+if (require.main === module) {
+    app.listen(port, () => {
+        console.log(`Server running on port ${port}`);
+    });
+}
+
+module.exports = app;
