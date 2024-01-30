66516d1e279991419b266486a3077a0ddd58abfb
004Anuj <anuj.sahu@kalvium.community>
2024-01-30 14:59:27 +0530

    react app created

---
+++ fronEndStroies/src/Component/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styles from './Home.module.css';
+import { Link } from 'react-router-dom'
+
+function Home() {
+return (
+    <div>
+        <h1 className={styles.tag}>Welcome to the Stories</h1>
+        <div className={styles.inputBox}>
+            <input type="text" placeholder="Username" className={styles.input} />
+            <input type="password" placeholder="Password" className={styles.input} />
+        </div>
+        <div className={styles.buttons}>
+            <Link to={"/login"}><button className={styles.loginButton}>Login</button></Link>
+            <Link to={"/signup"}><button className={styles.signupButton}>Sign Up</button></Link>
+        </div>
+    </div>
+)
+}
+export default Home
